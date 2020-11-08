5ee08ea19d3b124cd0a88560aa5b98610825788d
kureharyosuke <ohhayo.com@gmail.com>
2020-11-09 06:51:03 +0900

    #2. Form, FormSignup, FormSuccess

---
+++ src/FormSignup.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function FormSignup() {
+    return (
+        <div className="form-content-right">
+            <form className="form">
+                <h1>Get started with us today! Create your account by filling out the information below.</h1>
+                <div className="form-inputs">
+                    <label htmlFor="username" className='form-label'>input</label>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+    export default FormSignup
